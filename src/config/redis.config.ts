82abd58ed44439cac5b6ae85ecd7de6bbc34276d
Bhupeshkr7 <114282181+Bhupeshkr7@users.noreply.github.com>
2026-06-09 02:23:55 +0530

    done

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -1,21 +1,18 @@
-import Redis, { RedisOptions } from 'ioredis';
+import Redis from 'ioredis';
 import Logger from '../utils/logge.util';
 
-const redisOptions: RedisOptions = {
-  host: process.env.REDIS_HOST || 'localhost',
-  port: parseInt(process.env.REDIS_PORT || '6379', 10),
+const logger = new Logger('RedisConfig');
 
+export const redisClient = new Redis("rediss://default:gQAAAAAAAjZFAAIgcDEzN2RlMzE2YTllZGU0MWUzYmUzNTM5NGIzMTdmNjM3ZQ@evident-mosquito-144965.upstash.io:6379", {
   connectTimeout: 5000,
   maxRetriesPerRequest: null,
   enableReadyCheck: true,
   retryStrategy(times) {
     if (times >= 5) return null;
     return Math.min(times * 1000, 5000);
-  }
-}
+  },
+});
 
-export const redisClient = new Redis(redisOptions);
-const logger = new Logger('RedisConfig');
 redisClient.on('connect', () => {
   logger.info('Connected to Redis');
 });
@@ -23,9 +20,9 @@ redisClient.on('connect', () => {
 redisClient.on('error', (err) => {
   logger.error('Redis connection error:', err);
 });
+
 redisClient.on('close', () => {
   logger.warn('Redis connection closed');
 });
 
 export default redisClient;
-
