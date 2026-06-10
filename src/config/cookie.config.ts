7e6beacf63f5a8a5d8a9161a942bd32ae9554204
Bhupeshkr7 <114282181+Bhupeshkr7@users.noreply.github.com>
2026-06-11 01:37:02 +0530

    samesite update

--- src/config/cookie.config.ts
+++ src/config/cookie.config.ts
@@ -1,10 +1,7 @@
-import { env } from "./env.config";
-
 export const cookieOptions = {
   httpOnly: true,
-  secure: env.APP_ENV === "production",
-  sameSite: "lax" as const,
+  secure: true,
+  sameSite: "none" as const,
   path: "/",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
-
